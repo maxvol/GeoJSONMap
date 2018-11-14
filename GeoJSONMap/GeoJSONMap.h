72f101205a85cf37244164fdd9e8100b17d3ae0a
Maxim Volgin <maxim.volgin@me.com>
2018-11-14 20:09:37 +0100

    initial

---
+++ GeoJSONMap/GeoJSONMap.h
@@ -0,0 +1,19 @@
+//
+//  GeoJSONMap.h
+//  GeoJSONMap
+//
+//  Created by Maxim Volgin on 14/11/2018.
+//  Copyright © 2018 Maxim Volgin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeoJSONMap.
+FOUNDATION_EXPORT double GeoJSONMapVersionNumber;
+
+//! Project version string for GeoJSONMap.
+FOUNDATION_EXPORT const unsigned char GeoJSONMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeoJSONMap/PublicHeader.h>
+
+
